f6b1f110dd7c031545082d34bb22cadae5190efb
Siyavash Hamdi <siyavashhamdi@gmail.com>
2022-06-14 12:05:45 +0430

    (feat) Added endpoint * to catch all requests.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
+import * as express from "express";
+
 const bootstrap = async () => {
-  console.log('Started!');
+  console.log("Started!");
+
+  const app = express();
+
+  app.get("*", function (req, res) {
+    console.log({ msg: "get_all", url: req.url, req, res });
+
+    res.end();
+  });
+
+  app.post("*", function (req, res) {
+    console.log({ msg: "post_all", url: req.url, req, res });
+
+    res.end();
+  });
+
+  app.listen(3000, () => console.log("Server is running on localhos:3000!"));
 };
 
 bootstrap();
